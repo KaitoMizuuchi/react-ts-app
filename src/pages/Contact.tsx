e13a75f43819c7de984a91b9980eb4e342270787
KaitoMizuuchi <kaito.mizuchi@g.dreamcareer.co.jp>
2024-09-04 12:13:44 +0900

    react-hook-formの追加

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,35 +1,17 @@
-import { Box, Button, Container, Stack, TextField } from '@mui/material'
+import ContactForm from '@/components/contact/ContactForm'
+import {  Container, } from '@mui/material'
 import React from 'react'
-import { Controller, SubmitHandler, useForm } from 'react-hook-form'
 
-interface FormInputType {
-  name: string;
-  email: string;
-}
+
+
 
 const Contact = () => {
-  const { handleSubmit, control } = useForm<FormInputType>()
-  
-  const onSubmit: SubmitHandler<FormInputType> = (data) => {
-    console.log(data);
-  }
+
 
 
   return (
     <Container sx={{my: 3}}>
-      <Box component="form" onSubmit={handleSubmit(onSubmit)} sx={{maxWidth: "500px", m:"auto"}}>
-        <Stack spacing={2}>
-          <Controller name="name" control={control} defaultValue="" render={({field}) => (
-            <TextField id="name" label="名前を入力" variant="outlined" color="primary" {...field} />
-          )}
-          />
-          <Controller name="email" control={control} defaultValue="" render={({field}) => (
-            <TextField id="email" label="メールアドレスを入力" variant="outlined" color="primary" {...field} />
-          )}
-          />
-          <Button type="submit" variant="contained">送信</Button>
-        </Stack>
-      </Box>
+      <ContactForm />
     </Container>
   )
 }
